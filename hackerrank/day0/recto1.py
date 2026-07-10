aedcf87fd8efdcc3e67b2f027beffd630ac99841
wilsonramirez975-lab <wilsonramirez975@gmail.com>
2026-07-10 03:56:56 +0000

    recto1 terminado

---
+++ hackerrank/day0/recto1.py
@@ -0,0 +1,18 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+j = int(input())
+
+# Read and save an integer, double, and String to your variables.
+e = float(input())
+t = input()
+
+# Print the sum of both integer variables on a new line.
+print(i + j)
+
+# Print the sum of the double variables on a new line.
+print(f"{d + e:.1f}")
+
+# Concatenate and print the String variables on a new line
+print(s + t)
